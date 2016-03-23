862166ce5396fc2fee77ea4b7da75677cd6311ef
msamoylenko <ghost1212@mail.ru>
2016-03-23 13:32:29 +0500

    Better graph enumerator

--- GraphSyncRange/Solver/GraphSyncRange.Solver/main.cpp
+++ GraphSyncRange/Solver/GraphSyncRange.Solver/main.cpp
@@ -490,12 +490,53 @@ namespace graph
 
 using namespace graph;
 
-void print(const vector<int>& v) {
-	for (int i = 0; i < v.size(); ++i) {
-		printf("%d ", v[i]);
+class GraphEnumerator {
+
+	int n, k;
+	vector<vector<int>> edgeVariants;
+	vector<int> currentId;
+
+public:
+	Graph Current;
+
+	GraphEnumerator(int nVertices, int outDegree)
+		:n(nVertices), k(outDegree), Current(nVertices) {
+		currentId.assign(n, 0);
+		GenerateEdgeVariants();
+		for (int v = 0; v < n; ++v) {
+			Current.edges[v] = edgeVariants[0];
+		}
 	}
-	printf("\n");
-}
+
+	bool MoveNext() {
+		for (int v = 0; v < n; ++v) {
+			currentId[v] = (currentId[v] + 1) % edgeVariants.size();
+			Current.edges[v] = edgeVariants[currentId[v]];
+			if (currentId[v] != 0)
+				return true;
+		}
+		return false;
+	}
+
+private:
+	void GenerateEdgeVariants() {
+		vector<bool> v(n + k - 1, false);
+		fill_n(v.begin() + k, n - 1, true);
+
+		do {
+			vector<int> edges;
+			int vertex = 0;
+			for (int i = 0; i < v.size(); ++i) {
+				if (v[i])
+					++vertex;
+				else
+					edges.push_back(vertex);
+			}
+			edgeVariants.push_back(edges);
+		}
+		while (next_permutation(v.begin(), v.end()));
+	}
+};
 
 bool NextGraph(Graph& graph) {
 	int n = graph.VerticesCount();
@@ -525,18 +566,21 @@ int main(void) {
 	freopen("output.txt", "wt", stdout);
 
 	int n, k;
-	//	cin >> n >> k;
-	n = 3;
-	k = 3;
+	cin >> n >> k;
+	//	n = 3;
+	//	k = 3;
 
 	util::Permutation::Generate(k);
-	auto g = StartGraph(n, k);
+
+	auto enumerator = GraphEnumerator(n, k);
 	do {
-		//		if (StrongConnectivityChecker(*g).Check()) {
-		FindSyncRange(*g);
-		//		}
+		auto& graph = enumerator.Current;
+		if (StrongConnectivityChecker(graph).Check()) {
+			FindSyncRange(graph);
+		}
 	}
-	while (NextGraph(*g));
+	while (enumerator.MoveNext());
+
 
 	return 0;
 }
