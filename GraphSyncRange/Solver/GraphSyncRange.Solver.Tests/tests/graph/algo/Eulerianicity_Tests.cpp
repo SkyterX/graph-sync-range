f99ff3a6720b53745cd87f3cc2288f05003ea0e2
msamoylenko <ghost1212@mail.ru>
2016-03-29 12:58:31 +0500

    Eulerianicity tests - fixed checker.

---
+++ GraphSyncRange/Solver/GraphSyncRange.Solver.Tests/tests/graph/algo/Eulerianicity_Tests.cpp
@@ -0,0 +1,52 @@
+#include <CppUnitTest.h>
+#include <test_tools/ToStringExtensions.hpp>
+#include <util/MathUtils.hpp>
+#include <test_tools/Message.h>
+#include <experimental/generator>
+#include <graph/Graph6Reader.h>
+#include <graph/algo/Eulerian.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace test_tools;
+using namespace graph;
+using namespace util;
+using namespace std::experimental;
+using namespace std;
+
+namespace graph_algo_tests
+{
+	TEST_CLASS(Eulerianicity_Tests) {
+	public:
+
+		static constexpr char* graphsFileName = R"(TestData\directed_6_2.d6)";
+
+		TEST_METHOD(Eulerianicity_GraphsFile_Test) {
+
+			Graph6Reader reader(graphsFileName);
+			vector<int> vertexDegreeDiffs;
+			int eulerianCount = 0, totalCount = 0;
+			while (reader.MoveNext()) {
+				auto& graph = reader.Current;
+				auto n = graph.VerticesCount();
+				vertexDegreeDiffs.assign(n, 0);
+				for (int v = 0; v < n; ++v) {
+					vertexDegreeDiffs[v] += graph.edges[v].size();
+					for (auto& to : graph.edges[v]) {
+						--vertexDegreeDiffs[to];
+					}
+				}
+				bool isEulerian = std::all_of(
+					vertexDegreeDiffs.begin(), vertexDegreeDiffs.end(),
+					[](const int x) {
+						return x == 0;
+					});
+				Assert::AreEqual(isEulerian, IsEulerian(graph));
+				eulerianCount += isEulerian;
+				++totalCount;
+			}
+
+			Message::WriteLineF("Total Eulerian graphs : %d of %d", eulerianCount, totalCount);
+		}
+
+	};
+}
--- GraphSyncRange/Solver/GraphSyncRange.Solver/src/graph/algo/Eulerian.cpp
+++ GraphSyncRange/Solver/GraphSyncRange.Solver/src/graph/algo/Eulerian.cpp
@@ -6,16 +6,16 @@ using namespace std;
 namespace graph
 {
 	bool IsEulerian(const Graph& graph) {
-		if (graph.OutDegree() % 2 == 1) return false;
 		int n = graph.VerticesCount();
-		vector<int> inDegree(n, 0);
+		vector<int> degreeDiff(n, 0);
 		for (int v = 0; v < n; ++v) {
+			degreeDiff[v] += graph.edges[v].size();
 			for (auto& to : graph.edges[v]) {
-				++inDegree[to];
+				--degreeDiff[to];
 			}
 		}
 		for (int v = 0; v < n; ++v) {
-			if (inDegree[v] % 2 == 1)
+			if (degreeDiff[v] != 0)
 				return false;
 		}
 		return true;
